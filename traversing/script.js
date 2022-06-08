f232881eff050e4a55be06c2826c834e102be7bb
Holger <holgeraliusklose@gmail.com>
2022-06-08 11:57:33 +0200

    traversing

--- traversing/script.js
+++ traversing/script.js
@@ -1,16 +1,41 @@
+// Traversing
 const buttons = document.querySelectorAll(".concert-list li button");
+
 buttons.forEach((button) => {
   button.addEventListener("click", selectConcert);
 });
 
-function selectConcert(event /*Event Object */) {
+function selectConcert(event /* Event Object */) {
   // das ausgewählte Konzert hervorheben
   const button = event.target;
-  // parent, egal auf welcher Ebene
+
+  // erster parent, egal auf welcher Ebene
   const concert = button.closest("li");
+
+  // Suchen innerhalb vom li
   const title = concert.querySelector(".title");
-  console.log("click", concert);
+
+  const canceled = concert.querySelector(".canceled");
+
+  // Prüfen auf Kind
+  const containsCanceled = concert.contains(canceled);
+  if (containsCanceled) return;
+
+  // Auf Übereinstimmung mit Selektor prüfen
+  const matchesCanceledSelector = concert.matches(".concert.canceled");
+  if (matchesCanceledSelector) return;
+
+  // Alle Kinder
+  const children = concert.children;
+  for (let i = 0; i < children.length; i++) {
+    console.log(children[i]);
+    children[i].style.color = "white";
+  }
+  console.log("click", children);
   concert.style.backgroundColor = "red";
   title.style.backgroundColor = "orange";
-  console.log("click", title);
+
+  // Vorheriges Element
+  const previous = concert.previousElementSibling;
+  previous.style.backgroundColor = "blue";
 }
