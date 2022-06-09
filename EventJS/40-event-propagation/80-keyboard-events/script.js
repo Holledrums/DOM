6bcffaff5861292f23d09ace983ab6894cfd549a
Holger <holgeraliusklose@gmail.com>
2022-06-09 11:16:53 +0200

    keyboard-events

--- EventJS/40-event-propagation/80-keyboard-events/script.js
+++ EventJS/40-event-propagation/80-keyboard-events/script.js
@@ -3,4 +3,20 @@ const box = document.querySelector("div");
 document.addEventListener("keyup", keyPress);
 function keyPress(event) {
   console.log(event.key);
+  const currentTop = parseInt(box.style.top) || 0;
+  const currentLeft = parseInt(box.style.left) || 0;
+
+  if (event.key === "ArrowUp") {
+    const newTop = currentTop - 10;
+    box.style.top = newTop + "px";
+  } else if (event.key === "ArrowDown") {
+    const newTop = currentTop + 10;
+    box.style.top = newTop + "px";
+  } else if (event.key === "ArrowLeft") {
+    const newLeft = currentLeft - 10;
+    box.style.left = newLeft + "px";
+  } else if (event.key === "ArrowRight") {
+    const newLeft = currentLeft + 10;
+    box.style.left = newLeft + "px";
+  }
 }
