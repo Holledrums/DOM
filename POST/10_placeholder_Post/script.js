edef2e0cf7d82f54f0e065163ff552d2c1d058d2
Holger <holgeraliusklose@gmail.com>
2022-06-27 10:08:07 +0200

    placeholder Post

--- POST/10_placeholder_Post/script.js
+++ POST/10_placeholder_Post/script.js
@@ -1,1 +1,41 @@
-const apiURL = "https://jsonplaceholder.typicode.com/posts";
+const form = document.querySelector("form");
+const titleInput = document.querySelector("#title");
+const bodyInput = document.querySelector("#body");
+
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  console.log(posts);
+
+  posts.forEach((element) => {
+    const post = document.createElement("div");
+    const title = document.createElement("h2");
+    const content = document.createElement("p");
+
+    title.textContent = element.title;
+    content.textContent = element.body;
+
+    post.appendChild(title);
+    post.appendChild(content);
+
+    document.body.appendChild(post);
+  });
+}
+
+getPosts();
+
+form.addEventListener("submit", createPost);
+
+async function createPost(event) {
+  event.preventDefault();
+
+  const title = titleInput.value;
+  const postContent = bodyInput.value;
+
+  console.log(title, postContent);
+
+  // Daten an server schicken
+
+  await fetch("https://jsonplaceholder.typicode.com/posts");
+}
