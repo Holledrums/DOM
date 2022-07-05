7891005e288cfec4f15002cbbac53974555f0fa1
Holger <holgeraliusklose@gmail.com>
2022-07-05 13:41:15 +0200

    example CORS

--- async/05_CORS/script.js
+++ async/05_CORS/script.js
@@ -1,3 +1,4 @@
+const container = document.querySelector("#container");
 // IIFE
 (async () => {
   const corsProxy = "https://corsanywhere.herokuapp.com";
@@ -5,4 +6,31 @@
   const response = await fetch(`${corsProxy}/${apiURL}`);
   const posts = await response.json();
   console.log(posts);
+
+  posts.forEach((track) => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    const prefix = track.eId.substr(1, 2);
+    const trackId = track.eId.substr(4);
+
+    let trackURL;
+
+    switch (prefix) {
+      case "yt":
+        trackURL = "https://youtube.com/watch?v=" + trackId;
+
+        break;
+
+      default:
+        trackURL = "https://youtube.com/watch?v=" + track.eId;
+        break;
+    }
+
+    link.textContent = track.name;
+    link.href = trackURL;
+    link.setAttribute("target", "_blank");
+
+    li.appendChild(link);
+    container.appendChild(li);
+  });
 })();
